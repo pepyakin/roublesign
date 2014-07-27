1d8c5d1d0a90492dfbc7b499c8c6889579033a3a
Sergey Pepyakin <s.pepyakin@gmail.com>
2014-07-27 14:01:14 +0400

    Clean.

--- app/src/main/java/me/pepyakin/roublesign/MainActivity.java
+++ app/src/main/java/me/pepyakin/roublesign/MainActivity.java
@@ -5,8 +5,6 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 
 
@@ -19,16 +17,22 @@ public class MainActivity extends Activity {
 
         EditText priceInput = (EditText) findViewById(R.id.main_price_input);
 
-        Typeface roubleTypeface = Typeface.createFromAsset(getAssets(), "fonts/rouble2.ttf");
-
         String priceHint = getString(R.string.main_price_hint);
-        SpannableStringBuilder ssb = new SpannableStringBuilder(priceHint);
-        for (int i = 0; i < ssb.length(); i++) {
-            if (ssb.charAt(i) == '\u20BD') {
-                TypefaceSpan2 roubleTypefaceSpan = new TypefaceSpan2(roubleTypeface);
-                ssb.setSpan(roubleTypefaceSpan, i, i + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        CharSequence spannedPriceHint = spanWithRoubleTypeface(priceHint);
+        priceInput.setHint(spannedPriceHint);
+    }
+
+    private CharSequence spanWithRoubleTypeface(String priceHint) {
+        final Typeface roubleSupportedTypeface =
+                Typeface.createFromAsset(getAssets(), "fonts/rouble2.ttf");
+
+        SpannableStringBuilder resultSpan = new SpannableStringBuilder(priceHint);
+        for (int i = 0; i < resultSpan.length(); i++) {
+            if (resultSpan.charAt(i) == '\u20BD') {
+                TypefaceSpan2 roubleTypefaceSpan = new TypefaceSpan2(roubleSupportedTypeface);
+                resultSpan.setSpan(roubleTypefaceSpan, i, i + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
             }
         }
-        priceInput.setHint(ssb);
+        return resultSpan;
     }
 }
